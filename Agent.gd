88de73c618c23b69f0859484f17d1a98cacd85f5
Rafa Vital <2165692-rafavital@users.noreply.gitlab.com>
2024-02-17 13:40:41 -0300

    initial project setup

---
+++ Agent.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var initial_pos
+
+func _ready() -> void:
+	initial_pos = transform.origin
+
+
+func _physics_process(delta) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func reset() -> void:
+	transform.origin = initial_pos
+	print ("resetting")
+
+
+func _on_reach_target(body):
+	reset()
+
+
+func _on_out_of_bounds(body):
+	reset()
